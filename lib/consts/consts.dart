f3210f10caeda5c2d8b750d91f1f2bf39e822db6
kamrul1027 <122284715+kamrul1027@users.noreply.github.com>
2023-03-26 01:40:54 -0700

    Delete lib directory

--- lib/consts/consts.dart
+++
@@ -1,8 +0,0 @@
-export './colors.dart';
-export './colors.dart';
-export './images.dart';
-export './strings.dart';
-export './styles.dart';
-export './firebase_const.dart';
-export 'package:velocity_x/velocity_x.dart';
-export 'package:flutter/material.dart';
